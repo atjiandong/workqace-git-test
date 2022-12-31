e9d25298c7fec0f7dee5522c1c6d8f3a9b2f17c9
wanan <wanan@jiandong.com>
2022-12-31 20:53:00 +0800

    first commit

---
+++ src/main/java/com/atjiandong/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atjiandong.git;
+
+import sun.applet.Main;
+
+/**
+ * @author atjiandong
+ * @create 2022-12-31 20:47
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
+}
